7a94eec2d66191680d89e601f0f5cef52f5e8198
mkl <mkl@mikiaholding.dk>
2019-08-10 22:30:22 -0700

    updated crawler w. if

--- crawler.py
+++ crawler.py
@@ -1,25 +1,32 @@
 import requests
 urls_response = {}
 
-def checkRequestUrl(url):
-    request = requests.get(url)    
-    try:
-        urls_response[url] = request.status_code
-        # urls_response[url] = 404
+def checkRequestUrl(url):    
+    if("https" in url):
+        try:
+            request = requests.get(url)    
+            urls_response[url] = request.status_code
+        except:
+            urls_response[url] = 404
         print(urls_response)
-    except:
-        urls_response[url] = 404
+
+    else:
+        print("not today! hosie!")
 
 
 checkRequestUrl('https://api.github.com')
 checkRequestUrl('https://NotRealAtlltFUck.com')
+checkRequestUrl('I AM NOT FUCKIGN REAL ? ')
 
 
 
 
 #make dict for all https:
-#use regular expression to find http://... 
+
 #go through 1 page and find all links
 #use selenium to go to next page
+
+
+
 
 
